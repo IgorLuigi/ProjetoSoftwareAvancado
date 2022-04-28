f442030e1a98940860b795ec5c0867bbd267f40b
IgorLuigi <igor.luigi@hotmail.com>
2022-04-27 22:36:46 -0300

    Projeto aula 27-04

--- aula/bin/aula.dart
+++ aula/bin/aula.dart
@@ -1,8 +1,10 @@
 import 'package:aula/Aula%2020-04/exemplo-aula/aula.dart' as aula;
 import 'package:aula/Aula%2020-04/exemplo-aula/atividade2.dart' as atividade2;
 import 'package:aula/Aula%2020-04/projeto/projeto.dart' as projeto;
+import 'package:aula/Aula 27-04/projeto/projeto.dart' as projeto27_04;
 
 void main(List<String> arguments) {
   //atividade2.show();
-  projeto.show();
+  //projeto.show();
+  projeto27_04.show();
 }
