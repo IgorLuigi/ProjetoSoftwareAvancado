a052942c0fe3d0b67ef76400ad89a6b661853418
IgorLuigi <48772389+IgorLuigi@users.noreply.github.com>
2022-05-18 22:44:13 -0300

    Aula 18-05

---
+++ test/aula_18-05_test.dart
@@ -0,0 +1,31 @@
+import 'package:test/test.dart';
+import 'package:aula/Aula 18-05/aula18-05.dart';
+
+main() {
+  group('Teste de verificação de horas trabalhadas\n\n', () {
+    test('Teste da verificação dos valores das horas trabalhadas', () {
+      expect(verificarValorHora(salario: 1000, horas: 200), 5);
+    });
+  });
+
+  group('Teste de cálculo de variados tipos de horas extras \n\n', () {
+    test('Teste de cálculo de hora extra comum', () {
+      expect(
+          verificarValorHoraExtraComum(
+              salario: 1000, horas: 200, horaExtra: 1.5),
+          7.5);
+    });
+
+    test('Teste de cálculo de hora extra noturna', () {
+      expect(
+          verificarValorHoraNoturna(salario: 1000, horas: 200, horaExtra: 1.7),
+          8.5);
+    });
+
+    // test('Teste de cálculo de hora extra em feriados', () {
+    //   expect(
+    //       verificarValorHoraExtraFeriado(salario: 1000, horas: 200, horaExtra: 2.0),
+    //       10.0);
+    // });
+  });
+}
